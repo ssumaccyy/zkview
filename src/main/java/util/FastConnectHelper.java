c29345e15d44b8e5a3a08acb1f97f40a2dee54f9
top <top@example.com>
2024-07-31 10:30:25 +0800

    优化连接记录

--- src/main/java/util/FastConnectHelper.java
+++ src/main/java/util/FastConnectHelper.java
@@ -3,15 +3,16 @@ package util;
 import dao.FastButtonCfg;
 
 import java.io.*;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FastConnectHelper {
   public static final String UUID_TMP_FILE_FAST_BUTTON = "A8E4F7C4-9E2C-4C11-9A4C-8E0F4F4F4F4F";
+  public static final int FAST_BUTTON_CFG_TRUNK_COUNT = 3;
 
   public static List<FastButtonCfg> fetchFastButtonCfg() {
-    var list = parseFastButtonCfg();
+    var list = parseAndMaintainFastButtonCfg();
     LinkedList<FastButtonCfg> fastButtonCfgList = new LinkedList<>(list);
     if (fastButtonCfgList.isEmpty()) {
       fastButtonCfgList.add(new FastButtonCfg("10.106.1.1", 12181));
@@ -27,22 +28,23 @@ public class FastConnectHelper {
   }
 
   public static void writeToTmpFile(String content) {
-    try{
+    try {
       String tmpDir = System.getProperty("java.io.tmpdir");
       File tmpFile = new File(tmpDir, UUID_TMP_FILE_FAST_BUTTON);
       if (!tmpFile.exists()) {
+        // noinspection ResultOfMethodCallIgnored
         tmpFile.createNewFile();
       }
-      FileWriter fw = new FileWriter(tmpFile);
-
+      FileWriter fw = new FileWriter(tmpFile, StandardCharsets.UTF_8, true);
       fw.write(content);
+      fw.write(Character.LINE_SEPARATOR);
       fw.close();
-    } catch (IOException e) {
+    } catch (IOException ignored) {
 
     }
   }
 
-  public static List<FastButtonCfg> parseFastButtonCfg() {
+  public static List<FastButtonCfg> parseAndMaintainFastButtonCfg() {
     String tmpDir = System.getProperty("java.io.tmpdir");
     File tmpFile = new File(tmpDir, UUID_TMP_FILE_FAST_BUTTON);
     if (!tmpFile.exists() || tmpFile.isDirectory()) {
@@ -51,13 +53,27 @@ public class FastConnectHelper {
     LinkedList<FastButtonCfg> cfgList = new LinkedList<>();
     try {
       BufferedReader br = new BufferedReader(new FileReader(tmpFile));
-      String line = br.readLine();
+      var readLines = new ArrayList<>(br.lines().collect(Collectors.toUnmodifiableList())); // 读出来的所有行，可能有重复
+      var noRepeatSet = new LinkedHashSet<>(readLines); // 去重之后的行
       br.close();
-      String[] sArray = line.split(";");
-      if (sArray.length == 0 || sArray.length > 2) {
+      var arrayList = new ArrayList<>(noRepeatSet);
+      Collections.reverse(arrayList); // 顺序翻转   // 1,2,3 -> 3,2,1
+      String[] sArray = arrayList.toArray(new String[0]);
+      if (sArray.length == 0) {
         return Collections.emptyList();
       }
+
+      // 如果行数过大就直接截断
+      if (sArray.length > FAST_BUTTON_CFG_TRUNK_COUNT) {
+        sArray = Arrays.copyOf(sArray, FAST_BUTTON_CFG_TRUNK_COUNT);
+      }
+
+
+      var toWrite = new ArrayList<String>(sArray.length);
       for (String s : sArray) {
+        if (s.isEmpty() || s.isBlank()) {
+          continue;
+        }
         String[] sArray2 = s.split(":");
         if (sArray2.length != 2) {
           return Collections.emptyList();
@@ -68,12 +84,19 @@ public class FastConnectHelper {
         ) {
           FastButtonCfg cfg = new FastButtonCfg(sArray2[0], Integer.parseInt(sArray2[1]));
           cfgList.add(cfg);
+          toWrite.add(s);
         }
       }
-
+      Collections.reverse(toWrite);
+      FileWriter fw = new FileWriter(tmpFile, false);
+      for (String toWriteStr : toWrite) {
+        fw.write(toWriteStr);
+        fw.write(Character.LINE_SEPARATOR);
+      }
+      fw.close();
+      return cfgList;
     } catch (IOException e) {
       return Collections.emptyList();
     }
-    return cfgList;
   }
 }
